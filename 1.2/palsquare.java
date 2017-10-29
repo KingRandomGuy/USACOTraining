f32bbe18e7dcc849a93f36e92e8ff03ecafdd89c
Jim James <jimtjames@gmail.com>
2017-10-28 20:46:57 -0500

    fix spacing

--- 1.2/palsquare.java
+++ 1.2/palsquare.java
@@ -7,10 +7,7 @@ TASK: palsquare
 */
 
 public class palsquare {
-	
 	public static char[] dict = {'0', '1', '2', '3', '4', '5', '6' ,'7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'};
-	
-	
 	public static String changeBase(int N, int base, StringBuilder sb) {
 		if(N==0) {
 			String s = sb.toString();
