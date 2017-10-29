d86eaa9a2edccf1f9f46aa9eef95fc72e6c8f040
Jim James <jimtjames@gmail.com>
2017-10-28 20:31:29 -0500

    remove unused variables, close f

--- 1.1/friday.java
+++ 1.1/friday.java
@@ -1,4 +1,3 @@
-import java.util.StringTokenizer;
 import java.io.*;
 
 /*
@@ -6,28 +5,26 @@ ID: kingrandomguy
 LANG: JAVA
 TASK: friday
 */
+
 public class friday {
 	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("friday.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("friday.out")));
 		int[] counts = new int[7];
 		int N = Integer.parseInt(f.readLine());
+		f.close();
 		int days=0;
 		for(int i=1900; i<1900+N; i++) {
 			int daysPerYear = 365;
 			if((i%4==0 && i%100!=0) || i%400==0) {
 				daysPerYear=366;
 			}
-			int jan12 = 12;
-				days+=12;
+			days+=12;
 			counts[days%7]++;
-			int feb12 = jan12+31;
 			days+=31;
 			counts[days%7]++;
-			int mar12 = feb12+28;
 			days+=28;
 			if(daysPerYear==366) {
-				mar12++;
 				days++;
 			}
 			counts[days%7]++;
