4831eeff1382cfb362bc1732f495973292bbc611
Jim James <jimtjames@gmail.com>
2017-10-28 20:28:12 -0500

    fix spacing on farmer

--- 1.2/milk2.java
+++ 1.2/milk2.java
@@ -6,6 +6,7 @@ ID: kingrandomguy
 LANG: JAVA
 TASK: milk2
 */
+
 class Farmer{
 	public int start;
 	public int end;
@@ -18,20 +19,20 @@ class Farmer{
 	}
 }
 class milk2 {
-	public static void sortStart(Farmer[] arr)
-    {
-        int n = arr.length;
-        for (int i = 0; i < n-1; i++)
-        {
-            int min_idx = i;
-            for (int j = i+1; j < n; j++)
-                if (arr[j].start < arr[min_idx].start)
-                    min_idx = j;
-            Farmer temp = arr[min_idx];
-            arr[min_idx] =  arr[i];
-            arr[i] =  temp;
-        }
-    }
+	public static void sortStart(Farmer[] arr) {
+        	int n = arr.length;
+        	for (int i = 0; i < n-1; i++) {
+        	    int min_idx = i;
+        	    for (int j = i+1; j < n; j++){
+        	        if (arr[j].start < arr[min_idx].start){
+        	            min_idx = j;
+			}
+		    }
+        	    Farmer temp = arr[min_idx];
+        	    arr[min_idx] =  arr[i];
+        	    arr[i] =  temp;
+        	}
+    	}
 	public static void main(String[] args) throws IOException{
 		BufferedReader f = new BufferedReader(new FileReader("milk2.in"));
 		PrintWriter out = new PrintWriter("milk2.out");
@@ -49,21 +50,21 @@ class milk2 {
 		f.close();
 		sortStart(arr);
 		int start = arr[0].start;
-        int end = arr[0].end;
-        int maxMilked = end-start;
-        int maxGap = 0;
-        for(int i=1; i<arr.length; i++) {
-            Farmer a = arr[i];
-            if (a.start <= end) {
-                end = Math.max(a.end, end);
-            } 
-            else {
-                maxMilked = Math.max(maxMilked, end-start);
-                maxGap = Math.max(maxGap, a.start-end);
-                start = a.start;
-                end = a.end;
-            }
-        }
+        	int end = arr[0].end;
+        	int maxMilked = end-start;
+        	int maxGap = 0;
+        	for(int i=1; i<arr.length; i++) {
+        	    Farmer a = arr[i];
+        	    if (a.start <= end) {
+        	        end = Math.max(a.end, end);
+        	    } 
+        	    else {
+        	        maxMilked = Math.max(maxMilked, end-start);
+        	        maxGap = Math.max(maxGap, a.start-end);
+        	        start = a.start;
+        	        end = a.end;
+        	    }
+        	}
 		out.println(maxMilked + " " + maxGap);
 		out.close();
 	}
